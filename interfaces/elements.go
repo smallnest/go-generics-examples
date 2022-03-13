fd2fd90dedb520291eed94e1e8329963e801a78b
chaoyuepan <chaoyuepan@baidu.com>
2022-03-13 20:00:11 +0800

    more changes

--- interfaces/elements.go
+++ interfaces/elements.go
@@ -4,6 +4,13 @@ import (
 	"io"
 )
 
+type MyInt int
+
+type I0 interface {
+	~MyInt
+	~error
+}
+
 // func I1[K any, V interface{ K }]() {
 // }
 
@@ -16,9 +23,12 @@ func I3[K any, V interface{ int | any }]() {
 // func I4[K any, V interface{ int | ~int }]() {
 // }
 
-type MyInt int
-
 func I5[K any, V interface{ int | MyInt }]() {
+}
+
+type MyInt2 = int
+
+func I5_2[K any, V interface{ int | MyInt2 }]() {
 }
 
 // func I6[K any, V interface{ int | ~MyInt }]() {
@@ -49,6 +59,9 @@ func I11[K interface {
 // func I13[K comparable | int]() {
 // }
 
+func I14[K interface{ comparable } | int]() {
+}
+
 var (
 	_ interface{}
 	_ interface{ m() }
@@ -69,20 +82,34 @@ var (
 	// _ struct{ i ~int }
 
 	// _ struct{ i constraints.Ordered }
+
+	_ interface{ any }
+	_ interface {
+		interface {
+			any
+			m()
+		}
+	}
+	_ interface {
+		interface {
+			int | ~int
+			m()
+		}
+	}
 )
 
 // type I14[K int, V any] interface {
 // 	interface{ K }
 // }
 
-// type Node interface {
-// 	Node
-// }
+type Node interface {
+	Node
+}
 
-// type Tree interface {
-// 	TreeNode
-// }
+type Tree interface {
+	TreeNode
+}
 
-// type TreeNode interface {
-// 	Tree
-// }
+type TreeNode interface {
+	Tree
+}
